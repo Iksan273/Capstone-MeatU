6030fd3cdec53e6401c3bcce77fc449a7bcb7e28
Rein30S <rickysebastian001@gmail.com>
2023-06-06 03:27:54 +0700

    ui login, register, dashboard, riwayat, detail riwayat
    Menambahkan activity dan xml login, register, dashboard, riwayat, detail riwayat

---
+++ app/src/main/java/com/example/meatuapp/Activity_Detail_Riwayat.kt
@@ -0,0 +1,11 @@
+package com.example.meatuapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_Detail_Riwayat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_riwayat)
+    }
+}
