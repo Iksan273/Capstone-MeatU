111ee1c903f820375078c7b06a999fda431f16d5
Thorxds <103805253+Thorxds@users.noreply.github.com>
2023-06-10 18:57:37 +0700

    app.py

--- meatU/app.py
+++ meatU/app.py
@@ -1,18 +1,19 @@
 from flask import Flask, jsonify, request, json
 from flask_sqlalchemy import SQLAlchemy
-import pymysql
+from flask_mysqldb import MySQL
 import bcrypt
 from flask_marshmallow import Marshmallow
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
 #configure connection Mysql
-db = pymysql.connect(
-    host = 'localhost',
-    user =  'root',
-    password = '',
-    database = 'meatu'
-)
+app.config['MYSQL_HOST'] = '34.101.128.44'
+app.config['MYSQL_USER'] = 'myuser'
+app.config['MYSQL_PASSWORD'] = 'mypass'
+app.config['MYSQL_DB'] = 'meatu'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+
 @app.route('/register', methods=['POST'])
 def register():
     # Get registration data from the request
@@ -26,10 +27,10 @@ def register():
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
     # Store the registration data in the database
-    cur = db.cursor()
+    cur = mysql.connection.cursor()
     cur.execute("INSERT INTO users (nama, alamat, email, password) VALUES (%s, %s, %s, %s)",
     (nama, alamat, email, hashed_password))
-    db.commit()
+    mysql.connection.commit()
 
     # Build the response
     response = {
@@ -49,41 +50,40 @@ def login():
     password = data['password']
 
     # Mengecek kecocokan email dan password di database
-    cur = db.cursor()
+    cur = mysql.connection.cursor()
     query = "SELECT * FROM users WHERE email=%s"
     cur.execute(query, (email,))
     user = cur.fetchone()
-
+    cur.close()
 
     # Menangani hasil autentikasi
     if user is None:
         return jsonify({'message': 'Email atau password salah'}), 401
-    hashed_password = user[4]
-    if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
-        # Mengambil data pengguna yang berhasil login
-        user_data = {
-        'id': user[0],
-        'nama': user[1],
-        'alamat': user[2],
-        'email': user[3]
-    }
-        return jsonify({'message':'Login berhasil'}, user_data), 200
+    if bcrypt.checkpw(password.encode('utf-8'), user['password'].encode('utf-8')):
+            # Data pengguna yang berhasil login
+            user_data = {
+                'id': user['id'],
+                'nama': user['nama'],
+                'alamat':user['alamat'],
+                'email': user['email']
+                }
+            return jsonify({'message':'Login berhasil'}, user_data), 200
     else:
         return jsonify({'message': 'Email atau password salah'}), 401
 
 @app.route('/users/<id>', methods=['GET'])
 def get_user(id):
-    cur = db.cursor()
+    cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM users WHERE id = %s", (id,))
     user = cur.fetchone()
     cur.close()
 
     if user:
         user_dict = {
-            'id': user[0],
-            'nama': user[1],
-            'alamat': user[2],
-            'email': user[3]
+            'id': user['id'],
+            'nama': user['nama'],
+            'alamat': user['alamat'],
+            'email': user['email']
         }
         return jsonify(user_dict)
     else:
@@ -91,7 +91,7 @@ def get_user(id):
 
 @app.route('/users/<id>/history', methods=['GET'])
 def get_user_history(id):
-    cur = db.cursor()
+    cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM history_user WHERE user_id = %s", (id,))
     history = cur.fetchall()
     cur.close()
@@ -100,11 +100,11 @@ def get_user_history(id):
         history_list = []
         for entry in history:
             history_dict = {
-                'id': entry[0],
-                'user_id': entry[1],
-                'link_gambar': entry[2],
-                'tanggal': entry[3].strftime('%Y-%m-%d'),
-                'hasil_prediksi': entry[4]
+                'id': entry['id'],
+                'user_id': entry['user_id'],
+                'link_gambar': entry['link_gambar'],
+                'tanggal': entry['tanggal'].strftime('%Y-%m-%d'),
+                'hasil_prediksi': entry['hasil_prediksi']
             }
             history_list.append(history_dict)
 
